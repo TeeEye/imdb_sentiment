cc505d402197ec00a12c8d28231bf80795a58928
TeeEye <wangchenghao@mokahr.com>
2019-06-19 13:52:00 +0800

    building

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
+import torch
 import torch.nn as nn
-
+from utils import *
 
 class SentimentNet(nn.Module):
     def __init__(self, embed_size, hidden_size, n_layers, bidirectional, weight, n_labels):
@@ -12,7 +13,13 @@ class SentimentNet(nn.Module):
         self.decoder = nn.Linear(hidden_size * (2 if bidirectional else 1), n_labels)
 
     def forward(self, x):
+        lens = torch.zeros(x.size(0), 1)
+        for i in range(x.size(0)):
+            j = x.size(1) - 1
+            while j > 0 and x[i][j] == 0:
+                j -= 1
+            lens[i][0] = j
         x = self.embedding(x)
-        outputs, _ = self.encoder(x)
+        outputs = run_rnn(self.encoder, x, lens)
         x = self.decoder(outputs[:, -1])
         return x
--- utils.py
+++ utils.py
@@ -55,3 +55,18 @@ def pad_sentences(sentences, max_len=500, pad_token=0):
             padded_sentence.append(pad_token)
         padded_sentences.append(padded_sentence)
     return padded_sentences
+
+
+def batch_select(tensor, index):
+    return tensor.gather(1, index.view(-1, 1, 1).expand(tensor.size(0), 1, tensor.size(2))).squeeze(1)
+
+
+def run_rnn(rnn, data, lens):
+    sorted_lens, indices = torch.sort(lens, descending=True)
+    _, desorted_indices = torch.sort(indices)
+    data = data.index_select(0, indices)
+    packed = nn.utils.rnn.pack_padded_sequence(data, sorted_lens, batch_first=True)
+    res, _ = rnn(packed)
+    padded, _ = nn.utils.rnn.pad_packed_sequence(res, batch_first=True, total_length=data.shape[1])
+    res = padded.index_select(0, desorted_indices).contiguous()
+    return batch_select(res, lens-1)
